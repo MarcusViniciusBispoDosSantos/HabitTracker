HabitRPG/habitica

Add avatar customizations to content API

common/script/content/appearance/hair/bangs.js
@@ -1,0 +1,8 @@
+import keys from '../keys.js';
+
+export default keys({
+  0: {},
+  1: {},
+  2: {},
+  3: {}
+});

common/script/content/appearance/hair/base.js
@@ -1,0 +1,19 @@
+import keys from '../keys.js';
+import sets from '../sets.js';
+export default keys({
+  0: {},
+  1: {},
+  2: {price: 2, set: sets.baseHair1},
+  3: {},
+  4: {price: 2, set: sets.baseHair1},
+  5: {price: 2, set: sets.baseHair1},
+  6: {price: 2, set: sets.baseHair1},
+  7: {price: 2, set: sets.baseHair1},
+  8: {price: 2, set: sets.baseHair1},
+  9: {price: 2, set: sets.baseHair2},
+  10: {price: 2, set: sets.baseHair2},
+  11: {price: 2, set: sets.baseHair2},
+  12: {price: 2, set: sets.baseHair2},
+  13: {price: 2, set: sets.baseHair2},
+  14: {price: 2, set: sets.baseHair2}
+});

common/script/content/appearance/hair/flower.js
@@ -1,0 +1,11 @@
+import keys from '../keys.js';
+
+export default keys({
+  0: {},
+  1: {},
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {}
+})

common/script/content/appearance/sets.js
@@ -1,0 +1,21 @@
+import t from '../translation';
+import keys from './keys.js';
+
+export default keys({
+  baseHair1: {setPrice: 5, text: t('hairSet1')},
+  baseHair2: {setPrice: 5, text: t('hairSet2')},
+  facialHair: {setPrice: 5, text: t('bodyFacialHair')},
+  specialShirts: {setPrice: 5, text: t('specialShirts')},
+  winterHairColors: {setPrice: 5, availableUntil: '2016-01-01'},
+  pastelHairColors: {setPrice: 5, availableUntil: '2016-01-01'},
+  rainbowHairColors: {setPrice: 5, text: t('rainbowColors')},
+  shimmerHairColors: {setPrice: 5, availableUntil: '2016-01-01', text: t('shimmerColors')},
+  hauntedHairColors: {setPrice: 5, availableUntil: '2016-01-01', text: t('hauntedColors')},
+  winteryHairColors: {setPrice: 5, availableFrom: '2015-12-18', availableUntil: '2016-02-02', text: t('winteryColors')},
+  rainbowSkins: {setPrice: 5, text: t('rainbowSkins')},
+  animalSkins: {setPrice: 5, text: t('animalSkins')},
+  pastelSkins: {setPrice: 5, availableUntil: '2016-01-01', text: t('pastelSkins')},
+  spookySkins: {setPrice: 5, availableUntil: '2016-01-01', text: t('spookySkins')},
+  supernaturalSkins: {setPrice: 5, availableUntil: '2016-01-01', text: t('supernaturalSkins')},
+  splashySkins: {setPrice: 5, availableUntil: '2016-01-01', text: t('splashySkins')}
+});

MarcusViniciusBispoDosSantos/HabitTracker

common/script/content/appearance/hair/bangs.js
@@ -1,0 +1,8 @@
+import prefill from '../prefill.js';
+
+export default prefill({
+  0: {},
+  1: {},
+  2: {},
+  3: {},
+});

common/script/content/appearance/hair/base.js
@@ -1,0 +1,19 @@
+import prefill from '../prefill.js';
+import sets from '../sets.js';
+export default prefill({
+  0: {},
+  1: {},
+  2: {price: 2, set: sets.baseHair1},
+  3: {},
+  4: {price: 2, set: sets.baseHair1},
+  5: {price: 2, set: sets.baseHair1},
+  6: {price: 2, set: sets.baseHair1},
+  7: {price: 2, set: sets.baseHair1},
+  8: {price: 2, set: sets.baseHair1},
+  9: {price: 2, set: sets.baseHair2},
+  10: {price: 2, set: sets.baseHair2},
+  11: {price: 2, set: sets.baseHair2},
+  12: {price: 2, set: sets.baseHair2},
+  13: {price: 2, set: sets.baseHair2},
+  14: {price: 2, set: sets.baseHair2},
+});

common/script/content/appearance/hair/flower.js
@@ -1,0 +1,11 @@
+import prefill from '../prefill.js';
+
+export default prefill({
+  0: {},
+  1: {},
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+});

common/script/content/appearance/sets.js
@@ -1,0 +1,21 @@
+import t from '../translation';
+import prefill from './prefill.js';
+
+export default prefill({
+  baseHair1: {setPrice: 5, text: t('hairSet1')},
+  baseHair2: {setPrice: 5, text: t('hairSet2')},
+  facialHair: {setPrice: 5, text: t('bodyFacialHair')},
+  specialShirts: {setPrice: 5, text: t('specialShirts')},
+  winterHairColors: {setPrice: 5, availableUntil: '2016-01-01'},
+  pastelHairColors: {setPrice: 5, availableUntil: '2016-01-01'},
+  rainbowHairColors: {setPrice: 5, text: t('rainbowColors')},
+  shimmerHairColors: {setPrice: 5, availableUntil: '2016-01-01', text: t('shimmerColors')},
+  hauntedHairColors: {setPrice: 5, availableUntil: '2016-01-01', text: t('hauntedColors')},
+  winteryHairColors: {setPrice: 5, availableFrom: '2015-12-18', availableUntil: '2016-02-02', text: t('winteryColors')},
+  rainbowSkins: {setPrice: 5, text: t('rainbowSkins')},
+  animalSkins: {setPrice: 5, text: t('animalSkins')},
+  pastelSkins: {setPrice: 5, availableUntil: '2016-01-01', text: t('pastelSkins')},
+  spookySkins: {setPrice: 5, availableUntil: '2016-01-01', text: t('spookySkins')},
+  supernaturalSkins: {setPrice: 5, availableUntil: '2016-01-01', text: t('supernaturalSkins')},
+  splashySkins: {setPrice: 5, availableUntil: '2016-01-01', text: t('splashySkins')},
+});